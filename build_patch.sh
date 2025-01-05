d00ec54ba9c09e46fc126ebf2a2d929fb41939b3
chopeks <37939738+chopeks@users.noreply.github.com>
2025-01-05 23:08:43 +0100

    fixed build_patch.sh when tag doesn't exist

--- build_patch.sh
+++ build_patch.sh
@@ -6,16 +6,22 @@ latest_tag=$(getBaseVersion)
 
 commit_hash=$(git rev-parse "$latest_tag")
 
-Source="${1-"$commit_hash"}" # commit hash o branch if you wanna overwrite
+if [ $? -ne 0 ]; then
+    commit_hash=""
+fi
+
+Source="${1-"has is $commit_hash"}" # commit hash o branch if you wanna overwrite
 
 zip_archive=$(artifactNameMod)
 
 7z a -tzip "$zip_archive" mod_legends preload scripts ui
-# Get a list of all files modified since that commit hash
-modified_files=$(git diff --name-only "$Source" HEAD)
-# Filter the files that are in 'gfx' or 'sounds' directories
-filtered_files=$(echo "$modified_files" | grep -E '^(gfx|sounds)/')
-# Add the filtered files into the existing zip archive
-echo "$filtered_files" | while read -r file; do
-    7z a "$zip_archive" "$file"
-done
+  # Get a list of all files modified since that commit hash
+  if [ -n "$commit_hash" ]; then
+  modified_files=$(git diff --name-only "$Source" HEAD)
+  # Filter the files that are in 'gfx' or 'sounds' directories
+  filtered_files=$(echo "$modified_files" | grep -E '^(gfx|sounds)/')
+  # Add the filtered files into the existing zip archive
+  echo "$filtered_files" | while read -r file; do
+      7z a "$zip_archive" "$file"
+  done
+fi
